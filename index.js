a015d701c8a530d2a14898a116cd85c09802fdf5
reza107-hub <rashidaminur104@gmail.com>
2023-05-06 15:46:09 +0600

    post user created git push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-
+const cors = require('cors');
 
 const users = [
     {
@@ -21,14 +21,21 @@ const users = [
     }
 ];
 
-
+app.use(cors())
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('users management running')
 })
 
-app.get('/users', (req , res) => {
+app.get('/users', (req, res) => {
     res.send(users)
 })
 
+app.post('/users', (req, res) => {
+    const newUser = req.body
+    newUser.id = users.length + 1
+    users.push(newUser)
+    res.send(newUser)
+})
 
 app.listen(port)
